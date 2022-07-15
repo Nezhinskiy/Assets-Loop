07237d7c7e9d955a868331d405754f47528a8550
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-07-15 12:43:19 +0300

    update parse.py

--- binance/parse_p2p/models.py
+++ binance/parse_p2p/models.py
@@ -7,6 +7,7 @@ User = get_user_model()
 PAYMENT_SYSTEMS = (
     ('', ''),
     ('Tinkoff', 'Tinkoff'),
+    ('Wise', 'Wise'),
 )
 
 
--- binance/parse_p2p/parse.py
+++ binance/parse_p2p/parse.py
@@ -2,34 +2,61 @@ import logging
 import time
 from http import HTTPStatus
 from os import getenv
+from pprint import pprint
 from sys import getsizeof
 from typing import Dict, List
 import requests
+assets = [
+    'USDT',
+    'BUSD',
+    'BTC',
+]
+trade_types = [
+    'BUY',
+    'SELL'
+]
+fiats = [
+    'RUB',
+    'USD',
+    'EUR',
+]
+pay_typeses = [
+    'Tinkoff',
+    'Wise',
+#    'TBCbank',
+#    'BankofGeorgia',
+    'RosBank',
+    'RUBfiatbalance',
+]
 
 ENDPOINT = 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search'
-stringData = {
-    "page": 1,
-    "rows": 20,
-    "publisherType": None,
-    "asset": "USDT",
-    "tradeType": "BUY",
-    "fiat": "RUB",
-    "payTypes": ["Tinkoff"]
-}
-
-HEADERS = {
-        "Content-Type": "application/json",
-        "Content-Length": str(getsizeof(stringData)),
-      }
 
 
-def get_api_answer():
+def create_data(asset, trade_type, fiat, pay_types):
+    PAGE = 1
+    ROWS = 1
+    data = {
+        "page": PAGE,
+        "rows": ROWS,
+        "publisherType": None,
+        "asset": asset,
+        "tradeType": trade_type,
+        "fiat": fiat,
+        "payTypes": [pay_types]
+    }
+    return data
+
+
+def get_api_answer(data):
     """Делает запрос к единственному эндпоинту API.
     Яндекс.Практикума.
     """
-
+    headers = {
+        "Content-Type": "application/json",
+        "Content-Length": str(getsizeof(data)),
+    }
     try:
-        response = requests.post(ENDPOINT, headers=HEADERS, json=stringData)
+        response = requests.post(ENDPOINT, headers=headers, json=data)
     except Exception as error:
         message = f'Ошибка при запросе к основному API: {error}'
         raise Exception(message)
@@ -38,5 +65,37 @@ def get_api_answer():
         raise Exception(message)
     return response.json()
 
-print(get_api_answer())
 
+def parse_price(response):
+    data = response.get('data')
+    if len(data) == 0:
+        price = None
+        return price
+    data1 = data[0]
+    adv = data1.get('adv')
+    price = adv.get('price')
+    return price
+
+
+# new_data = create_data(asset='USDT', trade_type='BUY', fiat='RUB', pay_types='Tinkoff')
+# response = get_api_answer(new_data)
+# price = parse_price(response)
+# print(price)
+
+def main():
+    for asset in assets:
+        for trade_type in trade_types:
+            for fiat in fiats:
+                for pay_types in pay_typeses:
+                    if fiat == 'RUB' and pay_types == 'Wise':
+                        continue
+                    new_data = create_data(asset, trade_type,
+                                           fiat, pay_types)
+                    response = get_api_answer(new_data)
+                    price = parse_price(response)
+                    print(f'{asset}, {trade_type}, {fiat}, '
+                          f'{pay_types}, {price}')
+
+
+if __name__ == '__main__':
+    main()
