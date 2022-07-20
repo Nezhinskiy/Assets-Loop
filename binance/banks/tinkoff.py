323bec7404ab13084b437b27977332a06af3395e
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-07-20 15:28:28 +0300

    update banks.tinkoff

--- binance/banks/tinkoff.py
+++ binance/banks/tinkoff.py
@@ -10,19 +10,20 @@ ENDPOINT = 'https://api.tinkoff.ru/v1/currency_rates?'
 
 
 def generate_params() -> List[dict[str]]:
-    fiats = [fiat[0] for fiat in FIATS]
-    fiats_combinations = tuple(combinations(fiats, 2))
+    fiats = [fiat[0] for fiat in FIATS]  # repackaging choices into a list
+    fiats_combinations = tuple(combinations(fiats, 2))  # 2: currency pair
     params_list = [dict([('from', params[0]), ('to', params[-1])])
                    for params in fiats_combinations]
+    # repackaging a list with tuples into a list with dicts
     return params_list
 
 
-def get_api_answer(params_list):
+def get_api_answer(params):
     """Делает запрос к единственному эндпоинту API.
     Яндекс.Практикума.
     """
     try:
-        response = requests.get(ENDPOINT, params=params_list)
+        response = requests.get(ENDPOINT, params=params)
     except Exception as error:
         message = f'Ошибка при запросе к основному API: {error}'
         raise Exception(message)
@@ -32,5 +33,14 @@ def get_api_answer(params_list):
     return response.json()
 
 
-for params_list in generate_params():
-    print(get_api_answer(params_list))
+def parce_price(response) -> dict:
+    """Iterates through all dictionaries until 'Premium' is found."""
+    payload = response['payload']
+    rates = payload['rates']
+    for category in rates:
+        if category['category'] == 'CUTransfersPremium':
+            return category
+
+
+for params in generate_params():
+    print(parce_price(get_api_answer(params)))
