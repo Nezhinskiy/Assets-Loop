346f528c0ee89cbef4e920077c2a745a183da738
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-10-17 21:47:40 +0400

    refactor name

--- arbitration/arbitration/wsgi.py
+++ arbitration/arbitration/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE', 'arbitration.arbitration.settings'
+    'DJANGO_SETTINGS_MODULE', 'arbitration.settings'
 )
 
 application = get_wsgi_application()
