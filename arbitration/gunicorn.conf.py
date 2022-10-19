61089d945a21976b049b0140df9d0b6fda7817f4
Mikhail Nezhinsky <m.nezhinsky@yandex.ru>
2022-10-19 17:30:13 +0400

    updated gunicorn.conf.py

--- arbitration/gunicorn.conf.py
+++ arbitration/gunicorn.conf.py
@@ -1,10 +1,10 @@
-from multiprocessing import cpu_count
-
-
-def max_workers():
-    return cpu_count()
-
-
-TIMEOUT = 600
-workers = max_workers()
-worker_class = 'gevent'
+# from multiprocessing import cpu_count
+#
+#
+# def max_workers():
+#     return cpu_count()
+#
+#
+# TIMEOUT = 600
+# workers = max_workers()
+# worker_class = 'gevent'
