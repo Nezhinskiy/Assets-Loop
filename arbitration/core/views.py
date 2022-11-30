26f19f4beace0c6443787fbab5dc2ad765779c0e
nezhinsky <m.nezhinsky@yandex.ru>
2022-11-30 18:20:55 +0400

    update git

--- arbitration/core/views.py
+++ arbitration/core/views.py
@@ -153,7 +153,8 @@ def start(request):
     )
     general_chord = chord(general_group,
                           end_all_exchanges.s())
-    if InfoLoop.objects.last().value == 0:
+    if (InfoLoop.objects.all().count() == 0
+            or InfoLoop.objects.last().value == 0):
         InfoLoop.objects.create(value=True)
         count_loop = 0
         while InfoLoop.objects.last().value == 1:
