bcb86cfe5aec2e080e05d3d98c28ed81f7751c09
nezhinsky <m.nezhinsky@yandex.ru>
2022-12-01 23:02:14 +0400

    static

--- arbitration/arbitration/settings.py
+++ arbitration/arbitration/settings.py
@@ -32,9 +32,10 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = ['*']
+#     os.getenv(
+#     "DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost"
+# ).split(",")
 
 
 # Application definition
