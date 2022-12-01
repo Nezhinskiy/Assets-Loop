4893801c7aa4a648b97c3e8228b9e16df0c6af85
nezhinsky <m.nezhinsky@yandex.ru>
2022-12-02 00:26:55 +0400

    static

--- arbitration/arbitration/settings.py
+++ arbitration/arbitration/settings.py
@@ -154,8 +154,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/django/binance_api/arbitration/static'
 # STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
