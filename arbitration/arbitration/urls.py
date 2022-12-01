016c7b5e1c0d751277d96a646a0bbf21c3f681fc
nezhinsky <m.nezhinsky@yandex.ru>
2022-12-01 22:26:34 +0400

    static

--- arbitration/arbitration/urls.py
+++ arbitration/arbitration/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
      path('select2/', include('django_select2.urls')),
@@ -24,6 +25,8 @@ urlpatterns = [
      path('', include('core.urls')),
      path('', include('crypto_exchanges.urls', namespace='crypto_exchanges')),
      path('', include('banks.urls', namespace='banks')),
+     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
  ]
 
 urlpatterns += staticfiles_urlpatterns()
