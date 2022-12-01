31d5498300bc7f06f0d33ef720600acb4779bc98
nezhinsky <m.nezhinsky@yandex.ru>
2022-12-02 00:20:39 +0400

    static

--- arbitration/arbitration/settings.py
+++ arbitration/arbitration/settings.py
@@ -156,7 +156,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
