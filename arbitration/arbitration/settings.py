7a8fcf099ddbb67c822a237bf5af8406d1fce45f
nezhinsky <m.nezhinsky@yandex.ru>
2022-12-02 00:28:13 +0400

    static

--- arbitration/arbitration/settings.py
+++ arbitration/arbitration/settings.py
@@ -154,7 +154,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/django/binance_api/arbitration/static'
 # STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
