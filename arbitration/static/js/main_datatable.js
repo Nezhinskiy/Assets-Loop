c2ced684d7eeff94403f29a7de552cbd0aaadf04
nezhinsky <m.nezhinsky@yandex.ru>
2023-03-15 16:26:53 +0400

    Update static

--- arbitration/static/js/main_datatable.js
+++ arbitration/static/js/main_datatable.js
@@ -191,10 +191,10 @@ $(document).ready(function () {
         'serverSide': true,
         'ajax': {
             url: $('#dynamicDatatable').data('url') + $('#dynamicDatatable').data('filter'),
-            dataSrc: 'data',
+            dataSrc: 'data'
         },
         "rowCallback": function( row, data, index ) {
-            $('body>.tooltip_table').remove();
+            $('.tooltip').remove();
             var updatedTime = updateTime(data.update.updated);
             if (updatedTime > 120) {
                 $('td', row).addClass('obsolete');
@@ -216,30 +216,37 @@ $(document).ready(function () {
             }},
         columns: [
             {
-                data: 'diagram',
-                orderable: true
-            },
-            {
-                data:null,
+                data: null,
                 render: function (data, type, row){
-                    return '<span class="d-inline-block" tabindex="0" data-placement="top" data-toggle="tooltip_table" title="Нажмите для подробной информации">' +
+                    return '<span class="d-inline-block" tabindex="0" data-placement="top" data-toggle="tooltip" title="Нажмите для подробной информации">' +
                              '<p type="button" data-toggle="modal" data-diagram="'+row.diagram+'" data-content="'+modalWrite(row)+'" data-target="#myModal">' +
-                               '<span style="color:'+colorSelector(row.marginality_percentage)+'">'+row.marginality_percentage+'% </span>' +
-                               '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">' +
-                                 '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>' +
-                                 '<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>' +
-                             '</svg>' +
+                               row.diagram +
                              '</p>' +
                            '</span>'
                 },
                 orderable: true
             },
             {
-                data:null,
+                data: null,
+                render: function (data, type, row){
+                    return '<span class="d-inline-block" tabindex="0" data-placement="top" data-toggle="tooltip" title="Нажмите для подробной информации">' +
+                             '<p type="button" data-toggle="modal" data-diagram="'+row.diagram+'" data-content="'+modalWrite(row)+'" data-target="#myModal">' +
+                               '<span style="color:'+colorSelector(row.marginality_percentage)+'">'+row.marginality_percentage+'% </span>' +
+                               '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">' +
+                                 '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>' +
+                                 '<path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>' +
+                               '</svg>' +
+                             '</p>' +
+                           '</span>'
+                },
+                orderable: true
+            },
+            {
+                data: null,
                 render: function (data, type, row){
                     return `<p class="fw-light fs-15">< ${updateTime(row.update.updated)} cек.</p>`
                 },
-                orderable: false
+                orderable: true
             },
         ]
     });
@@ -263,6 +270,6 @@ $("#myModal").on('show.bs.modal', function (e) {
 });
 
 $( document ).ajaxComplete(function( event, request, settings ) {
-    $('[data-toggle="tooltip"],[data-toggle="tooltip_table"]').not( '[data-original-title]'
+    $('[data-toggle="tooltip"]').not( '[data-original-title]'
     ).tooltip();
 });
